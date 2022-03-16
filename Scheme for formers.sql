246ae9ded60e433ba5a78f166273108f13026213
team-10a <101160661+team-10a@users.noreply.github.com>
2022-03-16 23:45:33 +0530

    Add files via upload

--- Scheme for formers.sql
+++ Scheme for formers.sql
@@ -1,67 +1,122 @@
-create database agriculture
+create database SchemesForFarmers
 
-Create table UserInfo (
-Email varchar(50) not null,
-Password varchar(10) not null );
-
-
-
-create table Farmer(
-Full_Name varchar(50),
-Contact_No int not null,
-Email_Id varchar(50),
-Address varchar (max) not null,
-city varchar(20) not null,
-State varchar(20) not null,
-Pincode int,
-Account_No integer not null,
-IFSC varchar(20),
-Adhar int,
-PAN varchar(20),
-Trader_License varchar(20),
-Password varchar(20),
+create table UserDetails(
+UserTypeId int primary key not null,
+UserType Varchar (20)
 );
 
+create table LoginInfo(
+EmailId varchar (100),
+Password varchar (30)
+);
 
+create table UserInfo(
+FullName varchar (20),
+ContactNo int not null,
+EmailId	varchar(100),
+Address	varchar(100),
+City varchar(20),
+State varchar(20),
+Pincode	int not null,
+Password varchar(30),
+PermittedtoSale varchar(20),
+UserId int primary key not null,
+UserTypeId int FOREIGN KEY REFERENCES UserDetails(UserTypeId)
+);
 
-create table Bidder(
-Full_Name varchar(50),
-Contact_No int not null,
-Email_Id varchar(50),
-Address varchar (max) not null,
-city varchar(20) not null,
-State varchar(20) not null,
-Pincode int,
-Account_No integer not null,
-IFSC varchar(20),
-Adhar int,
-PAN varchar(20),
-Trader_License varchar(20),
-Password varchar(20));
+create table BankDetails(
+AccountNo int not null,
+IFSCCode varchar(30),
+Adhar int not null,
+Pan	varchar(20),
+TraderLicense varchar(30),
+UserId int FOREIGN KEY REFERENCES UserInfo(UserId)
+);
 
-
-Create table CropBidDetails(
-Crop_Type varchar(20),
-Crop_Name varchar(20),
-Fertilizer_Type varchar(20),
+Create table PlaceSellRequest(
+CropType varchar(20) not null,
+CropName varchar(20),
+FertilizerType varchar(20),
 Quantity decimal,
-Soil_Ph_Certificate imagepath(),
-Sold_To,
+SoilPhCertificate varchar (max),
+Primary Key(CropType),
+UserId int FOREIGN KEY REFERENCES UserInfo(UserId)
+);
+
+Create table ViewSoldCropHistory(
 Date date,
-MSP decimal ,
-Solid_Price decimal,
-Total_Price decimal,
-Base_Price decimal,
-Current_Bid decimal,
-Bid_Amount decimal);
+CropName varchar(20),
+Quantity decimal (18,2),
+MSP decimal (18,2),
+SolidPrice decimal (18,2),
+TotalPrice decimal(18,2),
+UserId int FOREIGN KEY REFERENCES UserInfo(UserId)
+);
 
+Create table ViewMarketPlace(
+CropType varchar(20) not null,
+CropName varchar(20),
+BasePrice decimal(18,2),
+CurrentBid decimal(18,2),
+Primary Key(CropName),
+UserId int FOREIGN KEY REFERENCES UserInfo(UserId)
+);
 
+create table ApplyForPolicy(
+Season varchar(50),
+Year date,
+CropName varchar(20), 
+SumInsured decimal (18,2),
+Area decimal (18,2) ,
+Insurance_Company varchar(20),
+SumInsuredperhect decimal (18,2),
+SharePremium decimal (18,2),
+PremiumAmount decimal (18,2),
+Primary Key(CropName),
+PolicyNo int FOREIGN KEY REFERENCES ClaimInsurance(PolicyNo)
+);
 
+create table ClaimInsurance(
+ParticularsOfInsuree varchar (20),
+PolicyNo int not null, 
+InsuranceCompany varchar (20),
+NameOfInsuree varchar (20),
+SumInsured decimal (18,3),
+CauseOfLoss nvarchar(max),
+DateOfLoss date,
+Primary Key(PolicyNo)
+);
 
+create table BidderWelcomePage(
+CropName varchar (20),
+CropType varchar (20),
+BasePrice decimal (18,2),
+CurrentBid	decimal (18,2),
+Bidammount	decimal (18,2),
+UserId int FOREIGN KEY REFERENCES UserInfo(UserId)
+);
 
+create table CropType(
+CropTypeId int primary key not null,
+CropType varchar (30)
+);
 
+create table Sell(
+SellId int primary key not null,
+CropName varchar(20),
+FertilizerType varchar(30),
+Quantity decimal(18,2),
+BasePrice decimal (18,2),
+CropTypeId int FOREIGN KEY REFERENCES CropType(CropTypeId),
+UserId int FOREIGN KEY REFERENCES UserInfo(UserId)
+);
 
-
- 
-
+create table Bidding(
+BiddingId int primary key not null,
+BidAmt decimal (18,2),
+BidDate Date,
+is_BitStatus BIT,
+UserId int FOREIGN KEY REFERENCES UserInfo(UserId),
+SellId int FOREIGN KEY REFERENCES Sell(SellId)
+);
 
